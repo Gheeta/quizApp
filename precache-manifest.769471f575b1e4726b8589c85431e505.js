b202b2896f425bb6aa8420caa286d96f724d9dea
Gheeta <101050138+Gheeta@users.noreply.github.com>
2022-09-15 21:43:03 +0200

    Updates

---
+++ precache-manifest.769471f575b1e4726b8589c85431e505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dcfaff51b9ec419151c6ad69d85d2f1",
+    "url": "/quizApp/index.html"
+  },
+  {
+    "revision": "db54ac1f76694944d6b1",
+    "url": "/quizApp/static/css/main.35088b67.chunk.css"
+  },
+  {
+    "revision": "eb3ef48bc5b8c90f9751",
+    "url": "/quizApp/static/js/2.c051897e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizApp/static/js/2.c051897e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db54ac1f76694944d6b1",
+    "url": "/quizApp/static/js/main.e27edbb1.chunk.js"
+  },
+  {
+    "revision": "6a59e033bda474eead7f",
+    "url": "/quizApp/static/js/runtime-main.d1ad9148.js"
+  }
+]);
